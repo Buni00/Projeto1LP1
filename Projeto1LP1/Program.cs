c97273ce9bd507aeba6583f17c9afe2ec4bd1263
Buni00 <bruno_hrf@outlook.com>
2023-04-15 17:47:37 +0100

    Add UpdateBulbState method

--- Projeto1LP1/Program.cs
+++ Projeto1LP1/Program.cs
@@ -5,9 +5,21 @@ namespace Projeto1LP1
 {
     class Program
     {
+        private static Button button1;
+        private static Button button2;
+        private static Button button3;
+
+        private static Bulb bulb1;
+        private static Bulb bulb2;
+        private static Bulb bulb3;
+        
         static void Main(string[] args)
         {
             CreateBulbs();
+
+            UpdateBulbState();
+
+            
         }
 
 
@@ -17,14 +29,23 @@ namespace Projeto1LP1
         private static void CreateBulbs()
         {
             // Creates 3 bulbs
-            Bulb bulb1 = new Bulb(1);
-            Bulb bulb2 = new Bulb(2);
-            Bulb bulb3 = new Bulb(3);
+            bulb1 = new Bulb(1);
+            bulb2 = new Bulb(2);
+            bulb3 = new Bulb(3);
 
             // Create 3 buttons associated with different bulbs
-            Button button1 = new Button(new List<Bulb>() { bulb1 } );
-            Button button2 = new Button(new List<Bulb>() { bulb1, bulb2 });
-            Button button3 = new Button(new List<Bulb>() { bulb2, bulb3 });
+            button1 = new Button(new List<Bulb>() { bulb1 } );
+            button2 = new Button(new List<Bulb>() { bulb1, bulb2 });
+            button3 = new Button(new List<Bulb>() { bulb2, bulb3 });
+        }
+
+        private static void UpdateBulbState()
+        {
+            // Print out the state of each bulb
+            Console.WriteLine("Current bulb states:");
+            Console.WriteLine(bulb1);
+            Console.WriteLine(bulb2);
+            Console.WriteLine(bulb3);
         }
     }
 }
