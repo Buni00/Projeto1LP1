d987f51fd852e88cc9628bf3fa59ef07da98d6bf
Buni00 <bruno_hrf@outlook.com>
2023-04-15 17:32:42 +0100

    Add CreateBulbs method

--- Projeto1LP1/Program.cs
+++ Projeto1LP1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Projeto1LP1
 {
@@ -6,7 +7,24 @@ namespace Projeto1LP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CreateBulbs();
+        }
+
+
+        /// <summary>
+        /// Creates 3 bulbs and 3 buttons
+        /// </summary>
+        private static void CreateBulbs()
+        {
+            // Creates 3 bulbs
+            Bulb bulb1 = new Bulb(1);
+            Bulb bulb2 = new Bulb(2);
+            Bulb bulb3 = new Bulb(3);
+
+            // Create 3 buttons associated with different bulbs
+            Button button1 = new Button(new List<Bulb>() { bulb1 } );
+            Button button2 = new Button(new List<Bulb>() { bulb1, bulb2 });
+            Button button3 = new Button(new List<Bulb>() { bulb2, bulb3 });
         }
     }
 }
